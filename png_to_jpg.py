a9ab21a3a53c620d2fdd959050770cdd32ce4bb1
bjohan20 <bjohan20@student.aau.dk@ai-pilot.srv.aau.dk>
2021-04-30 18:50:59 +0200

    added multiprocessing and python map --> 2h to 15min

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -3,9 +3,22 @@
 from glob import glob                                                           
 import cv2 
 from tqdm import tqdm
+from multiprocessing import Pool
+import time
+from tqdm import *
 
 pngs = glob('./*.png')
 
-for j in tqdm(pngs):
+#for j in tqdm(pngs):
+#    img = cv2.imread(j)
+#    cv2.imwrite(j[:-3] + 'jpg', img)
+
+def _foo(j):
     img = cv2.imread(j)
     cv2.imwrite(j[:-3] + 'jpg', img)
+
+if __name__ == '__main__':
+    with Pool(processes=6) as p:
+         with tqdm(total=len(pngs)) as pbar:
+            for i, _ in enumerate(p.imap_unordered(_foo, pngs)):
+                pbar.update()
