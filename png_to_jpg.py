3c86509a5b62cd9a86d1a6d25f87591853100877
Bjarne Johannsen <42101377+bjajoh@users.noreply.github.com>
2021-04-30 18:53:34 +0200

    Update png_to_jpg.py

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -7,17 +7,16 @@ from multiprocessing import Pool
 import time
 from tqdm import *
 
+#read files and make list with all .pngs in directory
 pngs = glob('./*.png')
 
-#for j in tqdm(pngs):
-#    img = cv2.imread(j)
-#    cv2.imwrite(j[:-3] + 'jpg', img)
-
+#convert the image and write it do drive
 def _foo(j):
     img = cv2.imread(j)
     cv2.imwrite(j[:-3] + 'jpg', img)
 
 if __name__ == '__main__':
+    #6 threads parallelized map loop
     with Pool(processes=6) as p:
          with tqdm(total=len(pngs)) as pbar:
             for i, _ in enumerate(p.imap_unordered(_foo, pngs)):
